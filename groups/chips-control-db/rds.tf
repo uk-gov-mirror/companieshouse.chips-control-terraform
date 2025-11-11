35e268e99f2f6bd0bf5548316e786912e9603143
Dylan Sides <dsides@companieshouse.gov.uk>
2025-11-11 16:05:02 +0000

    Update oracledb_cloudwatch_alarms to v1.0.236 to implement new Oracle alarm and logging changes.

--- groups/chips-control-db/rds.tf
+++ groups/chips-control-db/rds.tf
@@ -147,7 +147,7 @@ module "rds_start_stop_schedule" {
 }
 
 module "rds_cloudwatch_alarms" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/oracledb_cloudwatch_alarms?ref=tags/1.0.181"
+  source = "git@github.com:companieshouse/terraform-modules//aws/oracledb_cloudwatch_alarms?ref=tags/1.0.236"
 
   for_each = var.rds_cloudwatch_alarms
 
